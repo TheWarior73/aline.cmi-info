aa65a6ce3e0f4492d915ac0a04b9afa0da311c85
Th3_Warior <81911921+TheWarior73@users.noreply.github.com>
2026-05-22 18:58:39 +0200

    removed that joke of a class

--- USMB/INFO102/TP2/script.js
+++
@@ -1,9 +0,0 @@
-var vitrine = document.querySelector(".vitrine");
-var icon = document.querySelector(".iconeMenu");
-console.log(icon, vitrine);
-
-icon.addEventListener("click", clickMenu);
-function clickMenu() {
-    vitrine.className = vitrine.className === 'vitrine afficheMenu' ? 'vitrine' : 'vitrine afficheMenu' ;
-    icon.className = icon.className === 'iconeMenu afficheMenu' ? 'iconeMenu' : 'iconeMenu afficheMenu';
-}
